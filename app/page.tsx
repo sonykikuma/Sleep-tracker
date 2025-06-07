20e36e0fd8af4129f7d3dd1c8f7da29e53d3bbfd
sonykikuma <er.abhasoni@gmail.com>
2025-06-07 15:17:57 +0530

    neon, clerk added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "./components/Guest";
 
-function HomePage() {
+async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Sleep Tracker</h1>
